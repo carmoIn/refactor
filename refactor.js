dab11a2ed5bf8cc58be4a881d0aa0c7884642242
Helcio <helcio@carmo.in>
2022-10-18 20:11:25 -0300

    refactor: remove duplicidade de código para função

--- refactor.js
+++ refactor.js
@@ -2,26 +2,22 @@ function returnRandomNumber() {
   return Math.random();
 }
 
-function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
+function returnRandomBetween0To100() {
+  return Math.round(returnRandomNumber() * 100)
+}
 
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
+function sumToRandomNumber(num) {
+  return `Seu número é ${returnRandomFrom0To100() + num}!`;
 }
 
 function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
+  return `Seu número é ${returnRandomFrom0To100() - num}!`;
 }
 
 function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
+  return `Seu número é ${returnRandomFrom0To100() * num}!`;
 }
 
 function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+  return `Seu número é ${returnRandomFrom0To100(num) / num}!`;
 }
